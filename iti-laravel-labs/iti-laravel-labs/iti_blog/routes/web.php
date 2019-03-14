45e536dd3e373c4d18e0b13e4dbf6b157ed75c9d
nada <nada_faroukk@yahoo.com>
2019-03-14 13:19:42 +0200

    edit feature added

--- iti-laravel-labs/iti-laravel-labs/iti_blog/routes/web.php
+++ iti-laravel-labs/iti-laravel-labs/iti_blog/routes/web.php
@@ -11,12 +11,6 @@
 |
 */
 
-Route::get('/posts/create', 'PostsController@create') -> name('posts.create');
-Route::post('/posts','PostsController@store')-> name('posts.store');
-Route::get('/posts/{post}/edit', 'PostsController@edit') -> name('posts.edit');
-Route::get('/posts', 'PostsController@index') -> name('posts.index');
-Route::put('/posts/{post}', 'PostsController@update') -> name('posts.update');
-
-
-
-
+Route::get('/', function () {
+    return view('welcome');
+});
